7ec236f7f9e60e9f13dc2b69f33410b6975ea4c0
altuninovich@gmail.ru <root@DESKTOP-EFJ8J1R.localdomain>
2020-11-21 21:53:47 +0300

    Updates

---
+++ precache-manifest.103f7514b1bf1f7d289d2ca6d5e6e295.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6621c04ed571704c5b3f63c435fc0a0",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "39b8a823de2795971705",
+    "url": "/demo/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "712087c4a3aa2b319365",
+    "url": "/demo/static/css/main.0bffdc08.chunk.css"
+  },
+  {
+    "revision": "39b8a823de2795971705",
+    "url": "/demo/static/js/2.5f88996b.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/demo/static/js/2.5f88996b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "712087c4a3aa2b319365",
+    "url": "/demo/static/js/main.c0b51703.chunk.js"
+  },
+  {
+    "revision": "2d0e9525a2af85039cdc",
+    "url": "/demo/static/js/runtime-main.af4ffa1e.js"
+  },
+  {
+    "revision": "a6049b08ebabff98f391b6ccaeb14df8",
+    "url": "/demo/static/media/thumb-251882.a6049b08.png"
+  }
+]);
